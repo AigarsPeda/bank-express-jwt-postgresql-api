f6fbbd7f3dcddb3de7b7cb6f5a8264d48f52e16f
Aigars Peda <aigarspeda@gmail.com>
2021-01-11 13:09:30 +0200

    added client table to db

--- src/database.sql
+++ src/database.sql
@@ -1,6 +1,15 @@
-CREATE DATABASE perntodo;
+CREATE DATABASE bank;
 
-CREATE TABLE todo(
-  todo_id SERIAL PRIMARY KEY,
-  description VARCHAR(255)
+
+
+
+-- users table
+CREATE TABLE client (
+  client_id serial PRIMARY KEY,
+	name VARCHAR ( 50 ) NOT NULL,
+  surname VARCHAR ( 50 ) NOT NULL,
+	password VARCHAR ( 255 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+  last_login TIMESTAMP
 );
