d9febe027eed5b1c9438437d184aeb666bacb256
Aigars Peda <aigarspeda@gmail.com>
2021-01-11 13:11:58 +0200

    signup client and saving him to db

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { poll } from "./db";
+import { validSignupUser } from "./utils/validUser";
+import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 
 const PORT = 8000;
 
@@ -12,17 +15,50 @@ app.use(express.json()); // req.body
 
 // ROUTES //
 app.get("/", async (req, res) => {
-  // try {
-  //   const { description } = req.body;
-  //   const newTodo = await poll.query(
-  //     "INSERT INTO todo (description) VALUES($1) RETURNING *",
-  //     [description]
-  //   );
-  //   res.json(newTodo.rows[0]);
-  // } catch (error) {
-  //   console.error(error.message);
-  // }
   res.status(400).json("Hello World!");
+});
+
+// create user
+app.post("/signup", async (req, res) => {
+  try {
+    const created_on = new Date();
+    const { email, password, name, surname } = req.body;
+
+    // validating email, password and user name
+    if (!validSignupUser(email, password, name, surname)) {
+      return res.status(400).json({ error: "provide valid data" });
+    }
+
+    // hash password
+    const hashPassword = await argon2.hash(password);
+
+    const newUser = await poll.query(
+      ` INSERT INTO client (name, surname, email, password, created_on) 
+        VALUES($1, $2, $3, $4, $5) 
+        RETURNING name, surname, email, created_on, last_login, client_id
+      `,
+      [
+        name.toLowerCase(),
+        surname.toLowerCase(),
+        email.toLowerCase(),
+        hashPassword,
+        created_on
+      ]
+    );
+
+    // sign jsonwebtoken to save it in front
+    // end identify user later
+    const token = jwt.sign({ user: newUser.rows[0] }, process.env.SECRET_KEY!);
+
+    // returning user and token
+    return res.status(200).json({
+      user: newUser.rows[0],
+      token: token
+    });
+  } catch (error) {
+    console.error("SIGNUP ERROR: ", error.message);
+    return res.json({ error: "user name or email already taken" });
+  }
 });
 
 app.listen(PORT, () => {
