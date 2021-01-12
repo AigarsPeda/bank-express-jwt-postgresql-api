9d301190e89a60238575b98d11fcd88234a925c9
Aigars Peda <aigarspeda@gmail.com>
2021-01-12 09:03:07 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from "express";
 import cors from "cors";
-import { poll } from "./db";
-import { validSignupUser } from "./utils/validUser";
-import argon2 from "argon2";
-import jwt from "jsonwebtoken";
+import express from "express";
+import { clientsAccount } from "./controllers/account/clientsAccount";
+import { createClient } from "./controllers/auth/createClient";
+import { loginClient } from "./controllers/auth/loginClient";
+import { authMiddleware } from "./middleware/authMiddleware";
 
 const PORT = 8000;
 
@@ -13,53 +13,16 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // req.body
 
-// ROUTES //
 app.get("/", async (req, res) => {
-  res.status(400).json("Hello World!");
+  res.status(200).json("Hello World!");
 });
 
-// create user
-app.post("/signup", async (req, res) => {
-  try {
-    const created_on = new Date();
-    const { email, password, name, surname } = req.body;
+// AUTH ROUTE
+app.post("/signup", createClient);
+app.post("/login", loginClient);
 
-    // validating email, password and user name
-    if (!validSignupUser(email, password, name, surname)) {
-      return res.status(400).json({ error: "provide valid data" });
-    }
-
-    // hash password
-    const hashPassword = await argon2.hash(password);
-
-    const newUser = await poll.query(
-      ` INSERT INTO client (name, surname, email, password, created_on) 
-        VALUES($1, $2, $3, $4, $5) 
-        RETURNING name, surname, email, created_on, last_login, client_id
-      `,
-      [
-        name.toLowerCase(),
-        surname.toLowerCase(),
-        email.toLowerCase(),
-        hashPassword,
-        created_on
-      ]
-    );
-
-    // sign jsonwebtoken to save it in front
-    // end identify user later
-    const token = jwt.sign({ user: newUser.rows[0] }, process.env.SECRET_KEY!);
-
-    // returning user and token
-    return res.status(200).json({
-      user: newUser.rows[0],
-      token: token
-    });
-  } catch (error) {
-    console.error("SIGNUP ERROR: ", error.message);
-    return res.json({ error: "user name or email already taken" });
-  }
-});
+// ROUTES WITH AUTHORIZATION
+app.get("/account", authMiddleware, clientsAccount);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
