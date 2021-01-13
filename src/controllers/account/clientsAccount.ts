3cace71569f6be709a3eb71b1438998ecb359d34
Aigars Peda <aigarspeda@gmail.com>
2021-01-13 07:22:53 +0200

    Delete clientsAccount.ts

--- src/controllers/account/clientsAccount.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import { RequestWithUser } from "../../types";
-
-export const clientsAccount = (req: RequestWithUser, res: Response) => {
-  if (!req.user) res.status(401).json("unauthorized!");
-
-  res.status(200).json({ user: req.user, message: "Your Account" });
-};
