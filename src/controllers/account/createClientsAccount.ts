7a846cd4ecf614721d728df08eb99219f7ee7703
Aigars Peda <aigarspeda@gmail.com>
2021-01-14 15:02:26 +0200

    Update createClientsAccount.ts

--- src/controllers/account/createClientsAccount.ts
+++ src/controllers/account/createClientsAccount.ts
@@ -8,14 +8,26 @@ export const createClientsAccount = async (
 ) => {
   if (req.user) {
     const { user } = req.user;
-    const { account_no, bank_name } = req.body;
+    const {
+      card_no,
+      bank_name
+    }: { card_no: number; bank_name: string } = req.body;
     // const sixRandomDigits = Math.floor(100000 + Math.random() * 900000);
+
+    if (card_no.toString().length < 16 || card_no.toString().length > 16) {
+      return res.status(400).json("cards numbers length wrong");
+    }
+
+    if (bank_name.trim().length < 2) {
+      return res.status(400).json("bank name to short wrong");
+    }
+
     try {
       await poll.query(
-        `insert into accounts(account_no, bank_name, client_id) 
+        `insert into cards(card_no, bank_name, client_id) 
          values($1, $2, $3)
         `,
-        [account_no, bank_name.toLowerCase(), user.client_id]
+        [card_no, bank_name.toLowerCase(), user.client_id]
       );
       res.status(201).json("created!");
     } catch (error) {
