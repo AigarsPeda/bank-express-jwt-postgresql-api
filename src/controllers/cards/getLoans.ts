9e9360ca2fb881188c783836646a51be69a89c6b
Aigars Peda <aigarspeda@gmail.com>
2021-01-18 09:28:19 +0200

    Update getLoans.ts

--- src/controllers/cards/getLoans.ts
+++ src/controllers/cards/getLoans.ts
@@ -6,39 +6,48 @@ import { RequestWithUser } from "../../types";
 export const getLoans = async (req: RequestWithUser, res: Response) => {
   if (req.user) {
     const { user } = req.user;
-    const { borrower_card_id } = req.params;
+    const { card_id } = req.params;
     const { start_date, end_date } = req.query;
 
     // check who owns this card
+    // there are two owners lender and borrower
     const owner = await poll.query(
       "select card_id from cards where card_id = $1 AND client_id = $2",
-      [parseInt(borrower_card_id), user.client_id]
+      [parseInt(card_id), user.client_id]
     );
 
     if (owner.rows.length) {
       // you are owner and can see results
       let result: QueryResult<any>;
+
       try {
         if (start_date && end_date) {
+          // this information can be accessed by two people
+          // borrower or
+          // lender
           result = await poll.query(
             `select to_char(loan_taken_date, 'DD-MM-YYYY') 
-             as formatted_date, loan_amount, is_paid, lender_card_id, remaining_loan_amount  
+             as formatted_date, loan_amount, is_paid, lender_card_id, remaining_loan_amount, loan_id  
              from loans where 
-             borrower_card_id=$1 
+             borrower_card_id = $1 
+             OR
+             lender_card_id = $1
              and to_char(loan_taken_date, 'DD-MM-YYYY') 
              between $2 and $3 order by loan_taken_date desc
             `,
-            [borrower_card_id, start_date, end_date]
+            [card_id, start_date, end_date]
           );
         } else {
           result = await poll.query(
             `select to_char(loan_taken_date, 'DD-MM-YYYY') 
              as formatted_date, loan_amount, is_paid, lender_card_id, remaining_loan_amount  
              from loans where 
-             borrower_card_id=$1 
+             borrower_card_id = $1
+             OR
+             lender_card_id = $1
              order by loan_taken_date desc
             `,
-            [borrower_card_id]
+            [card_id]
           );
         }
         // console.log(result);
