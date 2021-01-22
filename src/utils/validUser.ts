d37d179175afb40987411ef203de61f9c018c5c1
Aigars Peda <aigarspeda@gmail.com>
2021-01-22 10:26:26 +0200

    Update validUser.ts

--- src/utils/validUser.ts
+++ src/utils/validUser.ts
@@ -1,19 +1,10 @@
-export const validSignupUser = (
-  email: string,
-  password: string,
-  name: string,
-  surname: string
-) => {
-  const validEmail =
-    typeof email === "string" && email.trim() !== "" && email.includes("@");
-  const validPassword =
-    typeof password === "string" && password.trim() !== "" && email.length > 5;
+export const validSignupUser = (name: string, surname: string) => {
   const validUsername =
     typeof name === "string" && name.trim() !== "" && name.length > 3;
   const validSurname =
     typeof surname === "string" && surname.trim() !== "" && surname.length > 3;
 
-  return validPassword && validUsername && validSurname && validEmail;
+  return validUsername && validSurname;
 };
 
 export const validateEmail = (email: string) => {
@@ -24,6 +15,15 @@ export const validateEmail = (email: string) => {
     email.length > 5;
 
   return validEmail;
+};
+
+export const validatePassword = (password: string) => {
+  const validPassword =
+    typeof password === "string" &&
+    password.trim() !== "" &&
+    password.length > 5;
+
+  return validPassword;
 };
 
 export const validLoginUser = (password: string) => {
