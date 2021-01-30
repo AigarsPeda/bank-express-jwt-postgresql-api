2257fea58f21894bebbff04f236c0bdec21a5101
Aigars Peda <aigarspeda@gmail.com>
2021-01-30 08:49:44 +0200

    Update createClient.ts

--- src/controllers/auth/createClient.ts
+++ src/controllers/auth/createClient.ts
@@ -37,7 +37,7 @@ export const createClient = async (req: Request, res: Response) => {
     const newUser = await poll.query(
       ` INSERT INTO clients (name, surname, email, password, created_on, last_login) 
         VALUES($1, $2, $3, $4, $5, $5) 
-        RETURNING name, surname, email, created_on, client_id
+        RETURNING name, surname, email, created_on, client_id, clients_total_balance
       `,
       [
         name.toLowerCase(),
