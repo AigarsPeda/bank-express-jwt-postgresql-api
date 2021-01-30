e03cb23b8b6dd66ee230c4fcf0f0f87072e7ff13
Aigars Peda <aigarspeda@gmail.com>
2021-01-30 08:50:49 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { updateLoan } from "./controllers/cards/updateLoan";
 import cors from "cors";
 import express from "express";
 import { createClient } from "./controllers/auth/createClient";
 import { loginClient } from "./controllers/auth/loginClient";
+import { getAllTransactions } from "./controllers/cards/getAllTransactions";
 import { getClientsCards } from "./controllers/cards/getClientsCards";
 import { getLoans } from "./controllers/cards/getLoans";
 import { getTransactions } from "./controllers/cards/getTransactions";
@@ -10,6 +10,8 @@ import { postCard } from "./controllers/cards/postCard";
 import { postDeposit } from "./controllers/cards/postDeposit";
 import { postLoan } from "./controllers/cards/postLoan";
 import { postWithdraw } from "./controllers/cards/postWithdraw";
+import { updateLoan } from "./controllers/cards/updateLoan";
+import { getUser } from "./controllers/user/getUser";
 import { authMiddleware } from "./middleware/authMiddleware";
 
 const PORT = 8000;
@@ -35,10 +37,13 @@ app.post("/deposit/:card_id", authMiddleware, postDeposit);
 app.post("/withdraw/:card_id", authMiddleware, postWithdraw);
 app.post("/loan/:card_id", authMiddleware, postLoan);
 app.put("/loan/:loan_id", authMiddleware, updateLoan);
+app.get("/transactions/all_transactions", authMiddleware, getAllTransactions);
+app.get("/user", authMiddleware, getUser);
 
 // ROUTES WITH WITH AUTHORIZATION NEEDED AND POSSIBLE QUERIES
 // /transactions/:card_id?start_date=start_date&end_date=end_date
 app.get("/transactions/:card_id", authMiddleware, getTransactions);
+
 // /loans/:borrower_card_id?start_date=start_date&end_date=end_date
 app.get("/loans/:card_id", authMiddleware, getLoans);
 
