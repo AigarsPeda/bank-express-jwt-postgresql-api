3c1bb64ab8581f13482014ec38f7eed9d81c764a
Aigars Peda <aigarspeda@gmail.com>
2021-02-22 07:36:43 +0200

    Create getClientsCards.ts

---
+++ src/controllers/cards/card/getClientsCards.ts
@@ -0,0 +1,20 @@
+import { Response } from "express";
+import { poll } from "../../../db";
+import { RequestWithUser } from "../../../types";
+
+export const getClientsCards = async (req: RequestWithUser, res: Response) => {
+  if (req.user) {
+    const { user } = req.user;
+    try {
+      const result = await poll.query(
+        "SELECT * FROM cards WHERE client_id = $1",
+        [user.client_id]
+      );
+      res.status(200).json(result.rows);
+    } catch (error) {
+      res.status(404).json("not found!");
+    }
+  } else {
+    res.status(401).json("unauthorized!");
+  }
+};
